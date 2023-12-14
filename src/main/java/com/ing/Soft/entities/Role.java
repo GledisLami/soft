cc291dabbcec782f32eb82f114f69df4857cade8
Gledis Lami <lamigledi@gmail.com>
2023-12-14 19:34:38 +0100

    Added Role Entity

---
+++ src/main/java/com/ing/Soft/entities/Role.java
@@ -0,0 +1,18 @@
+package com.ing.Soft.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "role")
+@Data
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role")
+    private String role;
+
+}
