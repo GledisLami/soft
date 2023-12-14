e6c9987e2bcc40673a64009f8ccb967956dabfa3
Gledis Lami <lamigledi@gmail.com>
2023-12-14 20:29:05 +0100

    Added Course Entity

---
+++ src/main/java/com/ing/Soft/entities/Course.java
@@ -0,0 +1,26 @@
+package com.ing.Soft.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "course")
+@Data
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "time")
+    private String time;
+
+    @Column(name = "studentsNo")
+    private Integer studentsNo;
+}
