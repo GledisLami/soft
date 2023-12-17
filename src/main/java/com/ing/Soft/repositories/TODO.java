869f04488794a15a70679514fbaaf863a54a2b79
Gledis Lami <lamigledi@gmail.com>
2023-12-18 00:20:54 +0100

    Updated TODO

--- src/main/java/com/ing/Soft/repositories/TODO.java
+++ src/main/java/com/ing/Soft/repositories/TODO.java
@@ -1,4 +1,14 @@
 package com.ing.Soft.repositories;
 
 public interface TODO {
+
+    /*
+    FR1: TODO: Create a new account and sign in. - Gledis
+    FR2: TODO: -enroll, -unenroll, -getEnrolledCourses, -getCourseDto on StudentEnrollment classes
+
+    FR4: TODO: Restrict leaving a feedback based on enrollment and if a student has a feedback
+
+    FR6: TODO:View top-rated courses: A student can view the 8 top-rated courses.
+
+     */
 }
